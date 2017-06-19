782758250a6a5655aa8c65bc4092bdef4bc3d3f8
PromZona <PromZona>
2017-06-19 20:21:43 +0300

    add new move script. Change level.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float speed = 2.0f;
+
+    private Transform target;
+
+    private void Awake()
+    {
+        if (!target) target = FindObjectOfType<Move>().transform;
+    }
+
+    private void Update ()
+    {
+        Vector3 position = target.position;             position.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public static float speed = 3f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 directionX = transform.right * moveX;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + directionX, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 directionY = transform.up * moveY;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + directionY, speed * Time.deltaTime);
+        }
+    }
+}
