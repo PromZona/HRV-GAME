4a1c1cb67ef4bb311170797e1905172772c6e68f
PromZona <PromZona>
2017-06-25 13:17:05 +0300

    Camera fixed

---
+++ Assets/Scripts/Character/MouseFollower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour {
+
+    public float smooth = 2.0f;
+
+    void FixedUpdate()
+    {
+        var MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        MousePosition.z = -90;
+        Quaternion rot = Quaternion.LookRotation(transform.position - MousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+    }
+}
---
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int lives = 5;
+
+    void Update()
+    {
+        if (lives <= 0)
+        {
+            die();
+        }
+    }
+
+    public void die ()
+    {
+        Destroy(gameObject);
+    }
+}
