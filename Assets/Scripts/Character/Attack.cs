1e7f91550f12659d7701af2b0459ec6a57661bc8
PromZona <PromZona>
2017-06-25 13:32:47 +0300

    add Attack

---
+++ Assets/Scripts/Character/Attack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    bool IsEnemyInRad = false;
+    Enemy enemy;
+    [SerializeField]
+    int Damage = 1;
+
+    void Update()
+    {
+        AttackEnemy();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            IsEnemyInRad = true;
+            Debug.Log(IsEnemyInRad);
+            enemy = other.GetComponent<Enemy>();
+            //enemy.lives -= 1;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            IsEnemyInRad = false;
+            Debug.Log(IsEnemyInRad);
+        }
+    }
+
+    void AttackEnemy()
+    {
+        if (IsEnemyInRad == true && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            enemy.lives -= Damage;
+        }
+    }
+
+
+}
