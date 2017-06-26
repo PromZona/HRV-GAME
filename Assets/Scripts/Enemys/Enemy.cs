a825c4eda9950f404310369500c9f2f86feaa530
PromZona <PromZona>
2017-06-26 16:04:36 +0300

    add - enemy rotate forward to Player

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -14,6 +14,8 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    
+
     public void die ()
     {
         Destroy(gameObject);
---
+++ Assets/Scripts/Enemys/LookAtCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCharacter : MonoBehaviour {
+
+    public float smooth = 2.0f;
+    public GameObject Character;
+
+    void Start()
+    {
+        Character = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        var CharacterPosition = Character.transform.position;
+        CharacterPosition.z = -90;       
+        Quaternion rot = Quaternion.LookRotation(transform.position - CharacterPosition, Vector3.forward);
+        transform.rotation = rot;
+    }
+
+}
